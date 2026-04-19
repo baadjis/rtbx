c69ca5c1e4f91cca87d03ca362c59efa4b09ed19
baadjis <baadjisidy@gmail.com>
2026-04-19 10:22:52 +0200

    refactored builders

---
+++ src/app/studios/flyer/page.tsx
@@ -0,0 +1,6 @@
+// app/builders/flyer/page.tsx
+import { FlyerBuilder } from '@/components/Builders/Flyer';
+
+export default function FlyerPage() {
+  return <FlyerBuilder />;
+}
---
+++ src/components/Builders/Flyer/index.tsx
@@ -0,0 +1,4 @@
+// components/builders/flyer/index.ts
+export { default as FlyerBuilder } from './FlyerBuilder';
+export * from './templates';
+export * from './data';        // si tu veux créer un data.ts spécifique plus tard
