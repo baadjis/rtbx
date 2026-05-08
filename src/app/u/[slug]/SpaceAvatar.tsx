4a12e86c893ffdcc3b804a790d82ffdb97ce5830
baadjis <baadjisidy@gmail.com>
2026-05-08 08:31:58 +0200

    updated avatar

--- src/app/u/[slug]/SpaceAvatar.tsx
+++ src/app/u/[slug]/SpaceAvatar.tsx
@@ -83,7 +83,7 @@ export function SpaceAvatar({
       bg-white/5
       backdrop-blur-md
       transition-all duration-500
-      p-3
+      p-5
       group-hover:p-0
       ${current.inner}
     `}
