09cc23e5fbc07167bd132e9886d1afccb238f26e
baadjis <baadjisidy@gmail.com>
2026-05-30 21:21:35 +0200

    update ai chat

---
+++ src/app/ai/chat/[id]/page.tsx
@@ -0,0 +1,15 @@
+// app/ai/chat/[id]/page.tsx
+import { getLang } from '@/lib/lang/lang-getter';
+import { LangType } from '@/lib/lang/types';
+import MCPChatClient from '@/app/ai/McpChatClient';
+
+export default async function ChatPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const lang = await getLang() as LangType;
+
+  return <MCPChatClient lang={lang} chatId={id} />;
+}
