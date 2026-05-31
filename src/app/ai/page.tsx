fe049d0316064481a4d674ae8b5fe6a7dc0f56f7
baadjis <baadjisidy@gmail.com>
2026-05-31 11:24:08 +0200

    update ai chat

---
+++ src/app/ai/page.tsx
@@ -0,0 +1,9 @@
+import { getLang } from "@/lib/lang/lang-getter"
+import AIPageClient from "./AiPageClient"
+import { LangType } from "@/lib/lang/types"
+
+export default async function AiPage(){
+
+    const lang= (await getLang() ) as LangType
+    return <AIPageClient  lang={lang}/>
+}
