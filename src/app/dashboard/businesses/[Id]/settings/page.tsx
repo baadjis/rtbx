38ba6fbd5f89e40511ecebaac9256ee9ea0c655c
baadjis <baadjisidy@gmail.com>
2026-06-22 14:36:11 +0200

    updated business

--- src/app/dashboard/businesses/[Id]/settings/page.tsx
+++ src/app/dashboard/businesses/[Id]/settings/page.tsx
@@ -14,8 +14,9 @@ export default  async function BusinessSettingPage({ params }: { params: Promise
     const lang =await getLang() as LangType;
     const supabase = await createClient();
     const businesResponse =  await supabase.from('businesses').select('*').eq('id', Number(Id))
-  
+    console.log(businesResponse)
     const business =  businesResponse.data
+
     const t= DATA[lang]
 
 
